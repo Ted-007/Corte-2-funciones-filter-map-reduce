5ce4872df4df7cb5064354083cee33d481f21248
Teddy Poveda <72889000+Ted-007@users.noreply.github.com>
2024-10-18 11:20:25 -0500

    Refactor: change values for proof

--- corte 2/program.py
+++ corte 2/program.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
-# Lista de números de ejemplo
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Lista de números
+numeros = [1, 3, 6, 9, 12, 15, 17, 20, 23, 26]
 
 # Función para verificar si un número es impar
 def es_impar(x):
